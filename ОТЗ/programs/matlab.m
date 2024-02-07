863a4864c1efcdf88c67f36b30badc62e762e644
Maxnikit <max.nikit.03@gmail.com>
2024-02-07 19:22:26 +0300

    1 done

--- ОТЗ/programs/matlab.m
+++ ОТЗ/programs/matlab.m
@@ -5,24 +5,24 @@ originalImage = imread('Pic_pr3_1.bmp');
 % Переводим в HSV для упрощённой сегментации по цвету
 hsvImage = rgb2hsv(originalImage);
 
-%Определяем пороги для красного цвета
-hueThresholdLow = 0.0; 
-hueThresholdHigh = 0.1; 
+%Определяем пороги для голубого цвета
+hueThresholdLow = 0.5;    
+hueThresholdHigh = 0.67;   
 saturationThresholdLow = 0.5; 
-valueThresholdLow = 0.5;
+valueThresholdLow = 0.5; 
 
-% Создаём маски для красных обьектов
-redMask = (hsvImage(:,:,1) >= hueThresholdLow) & ...
+% Создаём маски для голубых обьектов
+blueMask = (hsvImage(:,:,1) >= hueThresholdLow) & ...
           (hsvImage(:,:,1) <= hueThresholdHigh) & ...
           (hsvImage(:,:,2) >= saturationThresholdLow) & ...
           (hsvImage(:,:,3) >= valueThresholdLow);
 
 % Производим морфологическое закрытие для избавления от щелей
 se = strel('disk', 3); 
-redMaskClosed = imclose(redMask, se);
+blueMaskClosed = imclose(blueMask, se);
 
 % Маркируем обьекты
-[labels, numObjects] = bwlabel(redMaskClosed, 8);
+[labels, numObjects] = bwlabel(blueMaskClosed, 8);
 
 % Измеряем площадь и индекс наименьшего обьекта
 objectMeasurements = regionprops(labels, 'Area', 'PixelIdxList');
@@ -37,11 +37,11 @@ imshow(originalImage);
 hold on;
 plot(boundary(:,2), boundary(:,1), 'k', 'LineWidth', 4); % Рисуем границы черным цветом
 
-% Отображение площади на каждом красном объекте
+% Отображение площади на каждом голубом объекте
 for k = 1:numObjects
     objectArea = objectMeasurements(k).Area;
     centroid = regionprops(labels == k, 'Centroid');
     text(centroid.Centroid(1), centroid.Centroid(2), num2str(objectArea), ...
-         'Color', 'w', 'FontSize', 10, 'FontWeight', 'bold');
+         'Color', 'w', 'FontSize', 12, 'FontWeight', 'bold');
 end
 hold off;
